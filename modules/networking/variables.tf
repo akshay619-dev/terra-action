3145dd2c838bc2e915aa1267a014a8f983d8f2a5
akshay619-dev <akshay.patil61@gmail.com>
2024-07-30 16:30:50 +0530

    updated default values for variable

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,26 +1,32 @@
 variable "environment" {
   description = "The Deployment environment"
+  default = "Development"
 }
 
 variable "vpc_cidr" {
   description = "The CIDR block of the vpc"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnets_cidr" {
   type        = list
   description = "The CIDR block for the public subnet"
+  default     = ["10.0.1.0/24"]
 }
 
 variable "private_subnets_cidr" {
   type        = list
   description = "The CIDR block for the private subnet"
+  default     = ["10.0.2.0/24"]
 }
 
 variable "region" {
   description = "The region to launch the bastion host"
+  default     = "ap-south-1"
 }
 
 variable "availability_zones" {
   type        = list
   description = "The az that the resources will be launched"
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "region" {
   description = "ap-south-1"
+  default = "ap-south-1"
 }
 
 variable "environment" {
   description = "The Deployment environment"
+  default = "Development"
 }
 
 //Networking
 variable "vpc_cidr" {
   description = "The CIDR block of the vpc"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnets_cidr" {
   type        = list(any)
   description = "The CIDR block for the public subnet"
+  default     = ["10.0.1.0/24"]
 }
 
 variable "private_subnets_cidr" {
   type        = list(any)
   description = "The CIDR block for the private subnet"
+  default     = ["10.0.2.0/24"]
 }
 
 variable "tf_sg_name" {
